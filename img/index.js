239f78a146a9bb285addf0a32991880980c879ff
js3867 <83634235+js3867@users.noreply.github.com>
2023-02-13 01:27:35 +0800

    set up new nextjs project

---
+++ img/index.js
@@ -0,0 +1,7 @@
+// index.js allows easy usage in other areas
+
+import logo from './logo.png'
+
+export default {
+    logo,
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
 import '@/styles/globals.css'
 
+//--INTERNAL IMPORTS
+// import { NavBar, Footer } from '../components/componentsindex'
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <div>
+            {/* <NavBar /> */}
+            <Component {...pageProps} />
+            {/* <Footer /> */}
+        </div>
+    )
 }
