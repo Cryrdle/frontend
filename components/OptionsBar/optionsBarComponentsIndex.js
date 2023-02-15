7f4dae39b6fe88958941c0f517e83dc691d993bb
js3867 <83634235+js3867@users.noreply.github.com>
2023-02-15 19:51:32 +0800

    added file structure for options pop-ups

---
+++ components/OptionsBar/optionsBarComponentsIndex.js
@@ -0,0 +1,7 @@
+import Leaderboard from './Leaderboard/Leaderboard'
+import Streak from './Streak/Streak'
+import Settings from './Settings/Settings'
+import Updates from './Updates/Updates'
+import Help from './Help/Help'
+
+export { Leaderboard, Streak, Settings, Updates, Help }
