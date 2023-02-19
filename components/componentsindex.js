64889a559e943457e385da431ea746c114f1575f
js3867 <83634235+js3867@users.noreply.github.com>
2023-02-20 02:17:46 +0800

    added helper/reminder to main page

--- components/componentsindex.js
+++ components/componentsindex.js
@@ -1,8 +1,7 @@
-import Button from './Button/Button'
 import Footer from './Footer/Footer'
 import HeroSection from './HeroSection/HeroSection'
 import GuessBar from './GuessBar/GuessBar'
 import Dropdown from './Dropdown/Dropdown'
-import WinScreen from './GuessBar/WinScreen/WinScreen'
+import VisualAid from './VisualAid/VisualAid'
 
-export { Button, Footer, HeroSection, GuessBar, Dropdown, WinScreen }
+export { Footer, HeroSection, GuessBar, Dropdown, VisualAid }
